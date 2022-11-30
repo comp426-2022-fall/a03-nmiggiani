2fc52918575595cc066dc6a2d3f3808fa792f167
nmiggiani <36214382+nmiggiani@users.noreply.github.com>
2022-11-30 15:46:07 -0500

    new works

--- bin/cli.js
+++ bin/cli.js
@@ -1,24 +1,8 @@
+#!/usr/bin/env node
+
+import { roll } from "../lib/roll.js";
 import minimist from 'minimist';
-import { roll } from '../lib/roll.js';
 
 const args = minimist(process.argv.slice(2));
 
-if (args.sides) {
-    var sides = args.sides
-} else {
-    var sides = 6;
-}
-
-if(args.dice) {
-    var dice = args.dice
-} else {
-    var dice = 2;
-} 
-
-if(args.rolls) {
-    var rolls = args.rolls
-} else {
-    var rolls = 1;
-}
-
-console.log(roll(sides, dice, rolls))
+console.log(JSON.stringify(roll(args.sides, args.dice, args.rolls)));
