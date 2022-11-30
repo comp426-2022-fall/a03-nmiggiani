a099bceed765e8f393a439b290fffbaa599eed22
nmiggiani <36214382+nmiggiani@users.noreply.github.com>
2022-11-30 09:18:32 -0500

    all files

---
+++ bin/cli.js
@@ -0,0 +1,24 @@
+import minimist from 'minimist';
+import { roll } from '../lib/roll.js';
+
+const args = minimist(process.argv.slice(2));
+
+if (args.sides) {
+    var sides = args.sides
+} else {
+    var sides = 6;
+}
+
+if(args.dice) {
+    var dice = args.dice
+} else {
+    var dice = 2;
+} 
+
+if(args.rolls) {
+    var rolls = args.rolls
+} else {
+    var rolls = 1;
+}
+
+console.log(roll(sides, dice, rolls))
