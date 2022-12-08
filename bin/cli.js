68565e5a50e7ce3931015ae2d16e04a59b3d5f60
nmiggiani <36214382+nmiggiani@users.noreply.github.com>
2022-12-07 22:41:12 -0500

    final

--- bin/cli.js
+++ bin/cli.js
@@ -4,6 +4,7 @@ import { roll } from "../lib/roll.js";
 import minimist from 'minimist';
 
 const args = minimist(process.argv.slice(2));
+
 const sides = args.sides || 6;
 const dice = args.dice || 2;
 const rolls = args.rolls || 1;
